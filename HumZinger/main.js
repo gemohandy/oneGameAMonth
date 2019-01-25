3e22f8c91a335954c43e4e6dfa3e441e3d862249
gemohandy <bennetteidsness@gmail.com>
2019-01-24 22:07:33 -0600

    Added Flying Bees (They don't do anything yet.) Also, updated JS to reflect image location change. The added bumblebee images made them numerous enough for me to not want them cluttering up the main folder.

--- HumZinger/main.js
+++ HumZinger/main.js
@@ -4,17 +4,60 @@ var eventCatcherDiv;
 
 var scaleFactor = 4
 
+var maxLives = 3;
+
 var imageList = [
     {name:"hummingBird",
     leadingZeros:4,
     frameCount:2,
-    fileExtension: ".png"}
+    fileExtension: ".png"},
+    {name:"bumblebee",
+    leadingZeros:4,
+    frameCount:6,
+    fileExtension:".png"
+    }
 ]
 
 var allImages = [];
 
 birdX = -200;
 birdY = -200;
+
+class movingThing {
+    constructor(startFrame, startY, movementVector, g){
+        this.startFrame = startFrame;
+        this.startY = startY;
+        this.startX = g.width * scaleFactor;
+        this.movementVector = movementVector;
+    }
+    findPosition(frame, g){
+        if(frame < this.startFrame){
+            return [this.startX + 200, this.startY]
+        }
+        var framesOnScreen = frame - this.startFrame
+        var deltaX = framesOnScreen * this.movementVector[0];
+        var deltaY = framesOnScreen * this.movementVector[1];
+        return [this.startX + deltaX, this.startY + deltaY]
+    }
+    draw(frame, g){
+        return;
+    }
+}
+
+class bee extends movingThing{
+    draw(frame, g, overG){
+        //bumblebee images are second in the image list.
+        var coordinates = this.findPosition(frame, overG);
+        if(coordinates[0] < -70 || coordinates[1] < 50 || coordinates[1] > g.height * scaleFactor + 50){
+            return false;
+        }
+        g.drawImage(allImages[1][frame%4], coordinates[0], coordinates[1])
+        return true;
+    }
+    onCollide(){
+        lives--
+    }
+}
 
 function preloader()
 {
@@ -25,7 +68,7 @@ function preloader()
             while(following.length < imageSet.leadingZeros){
                 following = "0" + following
             }
-            allImages[index].push(loadImage(imageSet.name + following + imageSet.fileExtension))
+            allImages[index].push(loadImage("images/" + imageSet.name + following + imageSet.fileExtension))
         }
     })
 }
@@ -38,8 +81,8 @@ function loadImage(imageName){
 
 function mouseMove(E){
     E = E || window.event;
-    birdX = E.pageX * scaleFactor - 200
-    birdY = E.pageY * scaleFactor - 200
+    birdX = E.pageX * scaleFactor
+    birdY = E.pageY * scaleFactor
 }
 
 function startLoading()
@@ -70,13 +113,29 @@ function hasLoaded()
     if (isEverythingLoaded()) // Check to see if all info is loaded
     {
         clearInterval(gameInterval);
+        readyObjects();
         startGame();
+    }
+}
+
+objects = [];
+
+function readyObjects(){
+    for(var i = 0; i < 20; i++){
+        objects.push(new bee(20 * i, 200, [-10,0], gameCanvasObj))
+        objects.push(new bee(20 * i, 400, [-10,0], gameCanvasObj))
+        objects.push(new bee(20 * i, 600, [-10,0], gameCanvasObj))
+        objects.push(new bee(20 * i, 800, [-10,0], gameCanvasObj))
+        objects.push(new bee(20 * i, 1000, [-10,0], gameCanvasObj))
+        objects.push(new bee(20 * i, 1200, [-10,0], gameCanvasObj))
+        objects.push(new bee(20 * i, 1400, [-10,0], gameCanvasObj))
     }
 }
 
 function startGame()
 {
     gameStartTime = Date.now()
+    lives = maxLives;
     gameInterval = setInterval(runGame, 25);
 }
 
@@ -85,10 +144,21 @@ function runGame()
     gameCanvas.clearRect(0,0,gameCanvasObj.width * scaleFactor, gameCanvasObj.height * scaleFactor)
     currentFrame = Math.floor((Date.now() - gameStartTime)/25)
     drawBird(gameCanvas, birdX, birdY, currentFrame)
+    objectsToDelete = [];
+    for(i = 0; i < objects.length; i++){
+        if(!objects[i].draw(currentFrame, gameCanvas, gameCanvasObj)){
+            objectsToDelete.push(i);
+        }
+    }
+    objectsToDelete.reverse().forEach(function(index){objects.splice(index, 1)})
+    if(lives < 0){
+        clearInterval(gameInterval);
+    }
 }
 
 function drawBird(g, x, y, f){
     //The bird is the first element in the imageList - all the information we need is in there.
+    //Note - drawing the bird centered at x and y, rather than at the corner. That way, the same values can be used in collision detection.
     drawFrame = f % imageList[0].frameCount;
-    g.drawImage(allImages[0][drawFrame], x, y)
+    g.drawImage(allImages[0][drawFrame], x - 200, y - 200)
 }
